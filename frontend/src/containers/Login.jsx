84048966a7672c44fa6f558b00e19d59f05a8b24
Teresa Aguirre <axlina90@gmail.com>
2020-06-05 16:44:03 -0400

    Fronted

--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -9,33 +9,37 @@ const Login = (props) => {
     }
 
     return(
-        <form className="form-signin" onSubmit={handleSubmit}>
-            <div className="text-center mb-4">
-                <img className="mb-4" src={logo} alt="" width="72" height="72" />
-                <h1 className="h3 mb-3 font-weight-normal">App SmartCode</h1>
-                <p>App Reactjs + Nodejs + MySql </p>
-            </div>
+        <div class="card form-signin" >
+            <div class="card-body">
+                <form onSubmit={handleSubmit}>
+                    <div className="text-center mb-4">
+                        <img className="mb-4" src={logo} alt="" width="72" height="72" />
+                        <h1 className="h3 mb-3 font-weight-normal">App SmartCode</h1>
+                        <p>App Reactjs + Nodejs + MySql </p>
+                    </div>
 
-            <div className="form-label-group">
-                <input type="email" id="mail" className="form-control" placeholder="Usuario" required autoFocus />
-                <label htmlFor="mail">Usuario</label>
-            </div>
+                    <div className="form-label-group">
+                        <input type="email" id="mail" className="form-control" placeholder="Usuario" required autoFocus />
+                        <label htmlFor="mail">Usuario</label>
+                    </div>
 
-            <div className="form-label-group">
-                <input type="password" id="pass" className="form-control" placeholder="Contraseña" required />
-                <label htmlFor="pass">Contraseña</label>
-            </div>
+                    <div className="form-label-group">
+                        <input type="password" id="pass" className="form-control" placeholder="Contraseña" required />
+                        <label htmlFor="pass">Contraseña</label>
+                    </div>
 
-            <button className="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesíón</button>
-            <div className="form-group mb-0 pt-4">
-				<div className="col-sm-12 text-center">
-						<Link className="text-decoration-none" to="/register">
-							<p >¿No tienes una cuenta? <br/>Regístrate Ahora</p>
-						</Link>
-				</div>
-			</div>
-            <p className="mt-5 mb-3 text-muted text-center">&copy; 2020</p>
-        </form>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesíón</button>
+                    <div className="form-group mb-0 pt-4">
+                        <div className="col-sm-12 text-center">
+                                <Link className="text-decoration-none" to="/register">
+                                    <p >¿No tienes una cuenta? <br/>Regístrate Ahora</p>
+                                </Link>
+                        </div>
+                    </div>
+                    <p className="mt-5 mb-3 text-muted text-center">&copy; 2020</p>
+                </form>
+            </div>
+        </div>
     );
 };
 
